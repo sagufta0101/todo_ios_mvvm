1d623ee701c60672b94bcba6d7b367bc25b4ee9a
sagufta0101 <spsysorexedge@gmail.com>
2024-12-30 12:44:15 +0530

    Initial Commit

---
+++ todo_ios_mvvm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo_ios_mvvm
+//
+//  Created by Administrator on 30/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo_ios_mvvm/todo_ios_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_ios_mvvmApp.swift
+//  todo_ios_mvvm
+//
+//  Created by Administrator on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct todo_ios_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
